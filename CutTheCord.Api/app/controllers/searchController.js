7efa3e72d1a8e6398486382ea64e63a15950351d
TRH\rhd5538 <tshuff@fbhealthplans.com>
2017-06-19 08:44:01 -0500

    removed dead code from show factory.

--- CutTheCord.Api/app/controllers/searchController.js
+++ CutTheCord.Api/app/controllers/searchController.js
@@ -1,9 +1,6 @@
-﻿app.controller("searchController",["$scope", "$http", "$location", "ShowFactory", function ($scope, $http, $location, ShowFactory) {
+﻿app.controller("searchController",["$scope", "$http", "$location", function ($scope, $http, $location) {
 
     $scope.welcome = "search controller";
-    ShowFactory.searchShows().then((shows) => {
-        $scope.shows = shows;
-        console.log("shows: ", shows);
-    });
+    
 
 }]);
