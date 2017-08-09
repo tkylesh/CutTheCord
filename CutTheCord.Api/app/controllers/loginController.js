17cf84d869f0eb7a2908b6a4819e5d10529680bd
TRH\rhd5538 <tshuff@fbhealthplans.com>
2017-08-09 15:24:25 -0500

    added sweetalert loading screen to login.

--- CutTheCord.Api/app/controllers/loginController.js
+++ CutTheCord.Api/app/controllers/loginController.js
@@ -1,5 +1,16 @@
 ﻿app.controller("loginController", ["$scope", "$http", "$location", function ($scope, $http, $location) {
     $scope.login = function () {
+        // show something to the user to remind them that patience is a virtue
+        swal({
+            title: "Logging in...",
+            type: "info",
+            showConfirmButton: false,
+            text: "One moment please",
+            timer: 3000,
+            allowOutsideClick: false,
+            allowEscapeKey: false
+        });
+
         $http({
             method: 'POST',
             url: "/Token",
@@ -14,12 +25,14 @@
         })
             .then(function (result) {
                 console.log("result=", result);
-
+                swal.close(); //closes the patience modal
                 sessionStorage.setItem('user', result.data.userName);
                 sessionStorage.setItem('token', result.data.access_token);
                 $http.defaults.headers.common['Authorization'] = `bearer ${result.data.access_token}`;
 
                 $location.path("/home");
+            }, function (error) {
+                swal("Login failed", error.statusText, "error");
             });
     };
 }]);
