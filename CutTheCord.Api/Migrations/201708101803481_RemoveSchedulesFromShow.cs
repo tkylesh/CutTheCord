59db8bc5b5c0c967ccdbb12b7be5c75e571274d1
TRH\rhd5538 <tshuff@fbhealthplans.com>
2017-11-22 10:52:02 -0600

    Added Calendar view, controller, route.

---
+++ CutTheCord.Api/Migrations/201708101803481_RemoveSchedulesFromShow.cs
@@ -0,0 +1,16 @@
+namespace CutTheCord.Api.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemoveSchedulesFromShow : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ CutTheCord.Api/app/controllers/calendarController.js
@@ -0,0 +1,30 @@
+﻿app.controller("calendarController",["$scope", "$http", "$location", function ($scope, $http, $location) {
+
+    $scope.welcome = "calendar controller";
+    $scope.schedules = [];
+
+    
+
+    var loadSchedule = () => {
+        var memberEmail = sessionStorage.getItem("user");
+        console.log("user: ", memberEmail);
+
+        var config = {
+            params: {
+                "email": memberEmail
+            }
+        };
+
+        $http.get('http://localhost:49348/api/member', config).then(
+            (result) => {
+                console.log("get member: ", result);
+                var memberId = result.data.Id;
+                $http.get(`http://localhost:49348/api/schedule/${memberId}`).then((result) => {
+                    console.log("get schedules for member: ", result);
+                    $scope.schedules = result.data;
+                });
+            });
+    };
+    loadSchedule();
+
+}]);
